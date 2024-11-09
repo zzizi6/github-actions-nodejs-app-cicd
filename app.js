5c83cefecb8b828f57d9f4c78bdcdc5524436edb
Namyun Kim <nykim@hansung.ac.kr>
2024-11-09 16:49:04 +0900

    update.app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server is running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = server;
