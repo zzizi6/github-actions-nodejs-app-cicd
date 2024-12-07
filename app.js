655b73db981e9a27510fe853b395742cedd1b0ac
zzizi <zzi_zi@naver.com>
2024-12-07 16:42:00 +0900

    update app.js file

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Node JS Server is  running at http://${hostname}:${port}/`);
+  console.log(`Node JS Server is running at http://${hostname}:${port}/`);
 });
 
 module.exports = server;
